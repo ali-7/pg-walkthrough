e656acd629a594487470b6c70351ef9562eedef9
ali <ali.7@outlook.com>
2019-08-13 06:46:46 +0300

    add-dynamic-query-and-route

--- walkthroughSteps/server/controllers/index.js
+++ walkthroughSteps/server/controllers/index.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const staticSuperHeroes = require('./static.js');
+const express = require("express");
+const staticSuperHeroes = require("./static");
+const dynamicSuperHeroes = require("./dynamic");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ ali: 'ali' });
+router.get("/", (req, res) => {
+  res.json({ ali: "ali" });
 });
 
-router.get('/static', (req, res) => {
+router.get("/static", (req, res) => {
   res.json(staticSuperHeroes);
+});
+
+router.get("/dynamic", (req, res) => {
+  dynamicSuperHeroes
+    .getData()
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = router;
